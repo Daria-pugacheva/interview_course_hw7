df1a8e4a642b66675b07d14d53d7b0e48b227264
Daria-pugacheva <dpugacheva@yandex.ru>
2022-02-02 20:09:48 +0300

    Initial commit

---
+++ src/main/java/ru/gb/pugacheva/interview_course_hw7/InterviewCourseHw7Application.java
@@ -0,0 +1,13 @@
+package ru.gb.pugacheva.interview_course_hw7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCourseHw7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewCourseHw7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/pugacheva/interview_course_hw7/InterviewCourseHw7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.pugacheva.interview_course_hw7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCourseHw7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
